9eb198ffa567d421e9e1856b49b8abda73712ed4
PhatJack <tienphatng.693@gmail.com>
2025-01-18 08:58:50 +0700

    Refactor project descriptions to use consistent heading levels and add reading time feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,18 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
 import mdx from "@astrojs/mdx";
+import { remarkReadingTime } from "./remark-reading.time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind({
-		
+
 	}), icon(), mdx()],
 	devToolbar: {
 		enabled: false,
 	},
+	markdown: {
+		remarkPlugins:
+			[remarkReadingTime],
+	}
 });
